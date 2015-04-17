8f6d7cd9413fa789412ddd51b6d6e43ad8dd060a
Anton Risberg Alaküla <antonswez@gmail.com>
2015-04-17 21:32:15 +0200

    Added some more controls

--- Game/core/src/com/yox89/ld32/screens/BaseScreen.java
+++ Game/core/src/com/yox89/ld32/screens/BaseScreen.java
@@ -4,30 +4,34 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.viewport.ScalingViewport;
 
 public abstract class BaseScreen extends InputAdapter implements Screen {
 
-	protected Stage mStage;
+	protected Stage mGameStage, mUiStage;
 	protected Array<Disposable> mDisposables;
-	Texture img;
+
+	protected static final float GAME_WORLD_WIDTH = 64f;
+	protected static final float GAME_WORLD_HEIGHT = 48f;
 
 	@Override
 	public void show() {
 		mDisposables = new Array<Disposable>();
-		mStage = new Stage();
+		mGameStage = manage(new Stage(new ScalingViewport(Scaling.none,
+				GAME_WORLD_WIDTH, GAME_WORLD_HEIGHT)));
+		mUiStage = manage(new Stage());
 
 		Gdx.input.setInputProcessor(this);
-		
-		init(mStage);
-		
+
+		init(mGameStage, mUiStage);
 	}
-	
-	protected abstract void init(Stage stage);
-	
+
+	protected abstract void init(Stage game, Stage ui);
+
 	protected <T extends Disposable> T manage(T res) {
 		mDisposables.add(res);
 		return res;
@@ -35,34 +39,33 @@ public abstract class BaseScreen extends InputAdapter implements Screen {
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(.3f, .3f, .3f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		mStage.act(delta);
-		mStage.draw();
+		mGameStage.act(delta);
+		mGameStage.draw();
+
+		mUiStage.act(delta);
+		mUiStage.draw();
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void hide() {
-		// TODO Auto-generated method stub
 
 	}
 
@@ -72,7 +75,6 @@ public abstract class BaseScreen extends InputAdapter implements Screen {
 			d.dispose();
 		}
 		mDisposables.clear();
-		mStage.dispose();
 	}
 
 }
--- Game/core/src/com/yox89/ld32/screens/ExampleScreen.java
+++ Game/core/src/com/yox89/ld32/screens/ExampleScreen.java
@@ -1,30 +1,52 @@
 package com.yox89.ld32.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 
 public class ExampleScreen extends BaseScreen {
 
-	Texture img;
-
 	@Override
-	protected void init(Stage stage) {
-		img = manage(new Texture("badlogic.jpg"));
+	protected void init(Stage game, Stage ui) {
+		final Texture img = manage(new Texture("badlogic.jpg"));
+		img.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 
-		ExampleActor actor = new ExampleActor(img);
-		stage.addActor(actor);
+		final ExampleActor actor = new ExampleActor(img);
+		game.addActor(actor);
 
-		actor.setPosition(200, 200);
-		actor.setSize(128, 128);
-		
+		final float size = Math.min(GAME_WORLD_WIDTH, GAME_WORLD_HEIGHT) / 3f;
+		actor.setSize(size, size);
+		actor.setPosition(GAME_WORLD_WIDTH / 2 - size, GAME_WORLD_HEIGHT / 2);
+
+		final float mvx = GAME_WORLD_WIDTH / 4;
 		actor.addAction(Actions.forever(Actions.sequence(
-				Actions.moveBy(100f, 0f, 2f, Interpolation.sine),
-				Actions.moveBy(-100f, 0f, 2f, Interpolation.sine))));
+				Actions.moveBy(mvx, 0f, 2f, Interpolation.sine),
+				Actions.moveBy(-mvx, 0f, 2f, Interpolation.sine))));
 
+		final Label label = new Label("Hello, I am ui", new LabelStyle(
+				new BitmapFont(), Color.CYAN)) {
+
+			@Override
+			public void act(float delta) {
+				super.act(delta);
+
+				setText(String
+						.format("%.0f | %.0f", actor.getX(), actor.getY()));
+			}
+		};
+		label.setPosition(50, 50);
+
+		ui.addActor(label);
 	}
 
 	private class ExampleActor extends Actor {
@@ -33,6 +55,21 @@ public class ExampleScreen extends BaseScreen {
 
 		public ExampleActor(Texture tex) {
 			mTexture = tex;
+		}
+
+		@Override
+		public void act(float delta) {
+			super.act(delta);
+
+			if (Gdx.input.isKeyPressed(Keys.UP)) {
+				moveBy(0f, delta * 10f);
+			} else if (Gdx.input.isKeyPressed(Keys.DOWN)) {
+				moveBy(0f, delta * -10f);
+			}
+
+			if (Gdx.input.isKeyJustPressed(Keys.SPACE)) {
+				addAction(Actions.rotateBy(90f, .25f, Interpolation.sine));
+			}
 		}
 
 		@Override
