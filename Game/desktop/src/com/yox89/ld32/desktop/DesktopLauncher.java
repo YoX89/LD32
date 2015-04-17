eb8e4e3ad9924778316859597fb9f0c087d880e5
Anton Risberg Alaküla <antonswez@gmail.com>
2015-04-17 18:06:54 +0200

    Initial LibGDX project

---
+++ Game/desktop/src/com/yox89/ld32/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.yox89.ld32.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.yox89.ld32.Gajm;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Gajm(), config);
+	}
+}
