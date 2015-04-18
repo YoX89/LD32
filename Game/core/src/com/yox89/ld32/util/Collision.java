c29ba70cb9451dab2f4171cab01b67914165286a
Jonathan Hagberg <jonathan.hagberg@tactel.se>
2015-04-18 12:37:03 +0200

    Added collision flags for None and ghost

--- Game/core/src/com/yox89/ld32/util/Collision.java
+++ Game/core/src/com/yox89/ld32/util/Collision.java
@@ -2,6 +2,8 @@ package com.yox89.ld32.util;
 
 public class Collision {
 
-	public static final short PLAYER = 1 << 0,
-			WORLD = 1 << 1;
+	public static final short NONE = 0,
+			PLAYER = 1 << 0,
+			WORLD = 1 << 1,
+			GHOST = 1 << 2;
 }
